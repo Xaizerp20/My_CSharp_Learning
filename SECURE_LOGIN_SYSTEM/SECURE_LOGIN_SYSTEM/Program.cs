9fad1588a29f356d0f829d69396d27f33c9fbb70
Xavier_Jimenez <xavierjp1220@gmail.com>
2023-08-22 20:01:03 -0400

    Created Secure Login Proyect

---
+++ SECURE_LOGIN_SYSTEM/SECURE_LOGIN_SYSTEM/Program.cs
@@ -0,0 +1,18 @@
+﻿/* In this project, you will develop a user registration and access system. 
+    Users will be able to register with a username and password, 
+    and then log in to the system using their credentials. 
+    Passwords will be securely stored using encryption techniques. */
+
+
+namespace MyApp 
+{
+    using System;
+    using BCrypt.Net;
+
+    public class SECURE_LOGIN_SYSTEM
+    {
+       
+    }
+}
+
+   
